76a24699ee989ed76c895d1b75b2a70a24ef1552
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2025-12-24 13:09:53 +0530

    inngest-running-workflows

--- src/inngest/functions/index.ts
+++ src/inngest/functions/index.ts
@@ -1,4 +1,8 @@
 import { inngest } from "@/inngest/client";
+import prisma from "@/lib/db";
+import { indexCodebase } from "@/module/ai/rag";
+import { getRepoFileContents } from "@/module/github/github";
+import { success } from "better-auth";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -6,5 +10,36 @@ export const helloWorld = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("wait-a-moment", "1s");
     return { message: `Hello ${event.data.email}!` };
-  },
+  }
+);
+
+export const indexRepo = inngest.createFunction(
+  { id: "index-repo" },
+  { event: "repository-connected" },
+
+  async ({ event, step }) => {
+    const { owner, repo, userId } = event.data;
+
+    //=========== FILES =============
+    const files = await step.run("fetch-files", async () => {
+      const account = await prisma.account.findFirst({
+        where: {
+          userId: userId,
+          providerId: "github",
+        },
+      });
+
+      if (!account?.accessToken) {
+        throw new Error("No account found");
+      }
+
+      return await getRepoFileContents(account.accessToken, owner, repo);
+    });
+
+    await step.run("index-codebase", async () => {
+      await indexCodebase(`${owner}/${repo}`, files);
+    });
+
+    return { success: true, indexedFiles: files.length };
+  }
 );
--- src/module/github/github.ts
+++ src/module/github/github.ts
@@ -264,3 +264,69 @@ export const deleteWebhook = async (owner: string, repo: string) => {
     return false;
   }
 };
+
+// =================================
+// GETTING REPO ALL FILES (TEXT PART)
+// =================================
+
+export async function getRepoFileContents(
+  token: string,
+  owner: string,
+  repo: string,
+  path: string = ""
+): Promise<{ path: string; content: string }[]> {
+  const octokit = new Octokit({ auth: token });
+  const { data } = await octokit.rest.repos.getContent({
+    owner,
+    repo,
+    path,
+  });
+
+  // JUST A CHECK
+  if (!Array.isArray(data)) {
+    if (data.type === "file" && data.content) {
+      return [
+        {
+          path: data.path,
+          content: Buffer.from(data.content, "base64").toString("utf-8"),
+        },
+      ];
+    }
+    return [];
+  }
+
+  let files: { path: string; content: string }[] = [];
+
+  for (const item of data) {
+    if (item.type === "file") {
+      const { data: fileData } = await octokit.rest.repos.getContent({
+        owner,
+        repo,
+        path: item.path,
+      });
+
+      // CHECKING
+      if (
+        !Array.isArray(fileData) &&
+        fileData.type === "file" &&
+        fileData.content
+      ) {
+        // FILTER OUT NON-CODE FILES IF NEEDD (IMAGES ETC)
+        if (!item.path.match(/\.(png|jpg|jpeg|gif|ico|tar|gz|pdf|zip|svg)$/i)) {
+          files.push({
+            path: item.path,
+            content: Buffer.from(fileData.content, "base64").toString("utf-8"),
+          });
+        }
+      }
+    }
+
+    else if(item.type === "dir"){
+      const subFiles = await getRepoFileContents(token, owner, repo, item.path);
+
+      files = files.concat(subFiles);
+    }
+  }
+
+  return files;
+}
