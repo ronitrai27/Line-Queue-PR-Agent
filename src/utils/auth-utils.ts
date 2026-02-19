933514ad5e319ea254b87b6b5e1fcc8116c4a83c
Riteshkumar Lalankumar Sinha <ssaiet.ritesh@gmail.com>
2026-02-19 06:05:52 +0530

    Add OPENAI_KEY constant to auth-utils
    Added OPENAI_KEY constant to auth-utils.ts.

--- src/utils/auth-utils.ts
+++ src/utils/auth-utils.ts
@@ -2,13 +2,13 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-
+const OPENAI_KEY=sk_rhhfjr9576743848;
 export const requireAuth = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  if (!session) return redirect("/login")
+  if (!session) return redirect("/login");
 
   return session;
 };
