72ff0112d7b3ee77ecaa038b07d45a796f15a48a
ROX <108011380+ronitrai27@users.noreply.github.com>
2026-03-02 23:10:51 +0530

    just consoled.
    Remove commented-out console.log statement.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,7 @@
 
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-
+// console.log(prisma)
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
